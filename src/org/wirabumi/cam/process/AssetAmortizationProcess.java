8f550efd0f2a08a7d14595282cb0b329d7337b97
Zaein Aji Trahutomo <zaein.aji-trahutomo@hitachi-powergrids.com>
2021-03-25 16:09:06 +0800

    full fix for unimplemented method

--- src/org/wirabumi/cam/process/AssetAmortizationProcess.java
+++ src/org/wirabumi/cam/process/AssetAmortizationProcess.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 import org.apache.commons.lang.ObjectUtils;
 import org.apache.commons.lang.StringUtils;
 import org.hibernate.criterion.Restrictions;
+import org.openbravo.base.provider.OBProvider;
 import org.openbravo.base.util.ArgumentException;
 import org.openbravo.dal.service.OBCriteria;
 import org.openbravo.dal.service.OBDal;
@@ -29,7 +30,8 @@ import org.openbravo.service.db.DalConnectionProvider;
 import org.wirabumi.gen.oez.contract.Contract;
 
 public class AssetAmortizationProcess extends DalBaseProcess {
-
+  private final int januaryMonthNumber = 0;
+  
   @Override
   protected void doExecute(ProcessBundle bundle) throws Exception {
 
@@ -39,9 +41,6 @@ public class AssetAmortizationProcess extends DalBaseProcess {
     if (!asset.isDepreciate()){
     	bundle.setResult(OBErrorBuilder.buildMessage(null, "Warning", "asset is not set to be depreciated. No amortization generated for this asset."));
     }
-    
-    Result validation = isAssetEligibleForDepreciation(asset);
-    Contract.require(!validation.isError(), validation.getErrorMessage());
     
     AmortizationMethod amortizationMethod = gerAmortizationMethod(asset);
     switch (amortizationMethod) {
@@ -68,67 +67,121 @@ public class AssetAmortizationProcess extends DalBaseProcess {
   }
 
   private void handleDoubleDecliningAmortizatio(Asset asset) {
-    
-    //TODO implement this
-    /*
-     * 
-     * loop sd habis tahunnya:
-     *   tentukan tarifnya, jika tahun terakhir, maka tarif = sisa aset value dibagi dengan sisa bulan
-     *   loop sd end of year
-     *     buat amortization
-     *     consume asset value
-     * 
-     * commit
-     */
+    Result validation = isAssetEligibleForDepreciation(asset);
+    Contract.require(!validation.isError(), validation.getErrorMessage());
     
     removePendingAmortizationLine(asset);
     OBDal.getInstance().refresh(asset);
     
     Map<Date, Amortization> unProcessedAmortization = getUnprocessedAmortizationHeader(asset.getOrganization());
     
-    int yearLifeSpan = asset.getUsableLifeYears().intValue();
-    BigDecimal annualDepreciation = asset.getAnnualDepreciation().divide(new BigDecimal(100), 2, RoundingMode.HALF_DOWN);
-    BigDecimal assetValue = asset.getAssetValue();
-    Calendar amortizationDate = Calendar.getInstance();
-    amortizationDate.setTime(asset.getDepreciationStartDate());
+    BigDecimal annualDepreciationPercent = getAnnualDepreciationPercent(asset);
+    BigDecimal pendingAmortization = asset.getAssetValue();
     
-    for (int yearTh=1; yearTh<=yearLifeSpan-1; yearTh++){
-      BigDecimal tarif = 
-          assetValue
-            .divide(annualDepreciation, 2, RoundingMode.HALF_DOWN)
-            .divide(new BigDecimal(12), 2, RoundingMode.HALF_DOWN);
+    Calendar amortizationCalendar = Calendar.getInstance();
+    amortizationCalendar.setTime(asset.getDepreciationStartDate());
+    
+    int useLifeYear = asset.getUsableLifeMonths().intValue()/12;
+    for (int yearSeq=1; yearSeq<=useLifeYear; yearSeq++){
+      BigDecimal monthlyAmortizationTarif = getAmortizationTarif(pendingAmortization, annualDepreciationPercent, amortizationCalendar);
+      BigDecimal percentage = asset.getAssetValue().divide(monthlyAmortizationTarif, 2, RoundingMode.HALF_DOWN);
       
-      
-      Date amortizationStartDate = amortizationDate.getTime();
-
-      Amortization amortization = null;
-      if (unProcessedAmortization.containsKey(amortizationStartDate)) {
-        amortization = unProcessedAmortization.get(amortizationStartDate);
-      } else {
-        amortization = createAmortizationHeader(asset.getOrganization(), amortizationStartDate);
-        unProcessedAmortization.put(amortizationStartDate, amortization);
-      }
-
-      AmortizationLine amortizationLine = createAmortizationLine(asset, amortization, tarif);
-
-      OBDal.getInstance().save(amortizationLine);
-      
+      do{
+        Date amortizationStartDate = amortizationCalendar.getTime();
+        Amortization amortization = getAmortizationHeader(unProcessedAmortization, amortizationStartDate, asset);
+        AmortizationLine amortizationLine = createAmortizationLine(asset, amortization, monthlyAmortizationTarif, percentage);
+        
+        asset.getFinancialMgmtAmortizationLineList().add(amortizationLine);
+        amortization.getFinancialMgmtAmortizationLineList().add(amortizationLine);
+        OBDal.getInstance().save(amortizationLine);
+        OBDal.getInstance().save(asset);
+        OBDal.getInstance().save(amortization);
+        
+        amortizationCalendar.add(Calendar.MONTH, 1);
+        pendingAmortization = pendingAmortization.subtract(amortizationLine.getAmortizationAmount());
+      }while(isYearChanged(amortizationCalendar));
     }
     
     OBDal.getInstance().commitAndClose();
-
   }
-  
-  private AmortizationLine createAmortizationLine(Asset asset, Amortization amortization,
-      BigDecimal tarif) {
-    // TODO Auto-generated method stub
-    return null;
+
+  private BigDecimal getAmortizationTarif(BigDecimal assetValue,
+      BigDecimal annualDepreciationPercent, Calendar amortizationCalendar) {
+    BigDecimal yearlyAmortizationTarif = assetValue
+        .divide(annualDepreciationPercent, 2, RoundingMode.HALF_DOWN);
+    
+    BigDecimal monthlyAmortizationTarif = getAmortizationTarif(assetValue, annualDepreciationPercent, amortizationCalendar); 
+        yearlyAmortizationTarif
+        .divide(new BigDecimal(12-amortizationCalendar.get(Calendar.MONTH)), 2, RoundingMode.HALF_DOWN);
+        
+    return monthlyAmortizationTarif;
+  }
+
+  private Amortization getAmortizationHeader(Map<Date, Amortization> unProcessedAmortization, Date amortizationStartDate, Asset asset) {
+    if (unProcessedAmortization.containsKey(amortizationStartDate)) {
+      return unProcessedAmortization.get(amortizationStartDate);
+    } else {
+      Amortization result = createAmortizationHeader(asset.getOrganization(), amortizationStartDate);
+      unProcessedAmortization.put(amortizationStartDate, result);
+      
+      return result;
+    }
+  }
+
+  private boolean isYearChanged(Calendar amortizationCalendar) {
+    return amortizationCalendar.get(Calendar.MONTH)==januaryMonthNumber;
+  }
+
+  private BigDecimal getAnnualDepreciationPercent(Asset asset) {
+    return asset
+        .getAnnualDepreciation()
+        .divide(new BigDecimal(100), 2, RoundingMode.HALF_DOWN);
+  }
+
+  private AmortizationLine createAmortizationLine(Asset asset, Amortization header,
+      BigDecimal amount, BigDecimal amortizationPercentage) {
+    
+    AmortizationLine result = OBProvider.getInstance().get(AmortizationLine.class);
+    result.setAmortization(header);
+    result.setAsset(asset);
+    result.setCostcenter(asset.getCamCostcenter());
+    result.setAmortizationAmount(amount);
+    
+    long lineNo = header.getFinancialMgmtAmortizationLineList().size()+1;
+    result.setLineNo(lineNo);
+    
+    long assetSeqNo = asset.getFinancialMgmtAmortizationLineList().size()+1;
+    result.setSEQNoAsset(assetSeqNo);
+    
+    result.setAmortizationPercentage(amortizationPercentage);
+    result.setAmortizationAmount(amount);
+    result.setCurrency(asset.getCurrency());
+    
+    return result;
   }
 
   private Amortization createAmortizationHeader(Organization organization,
       Date amortizationStartDate) {
-    // TODO Auto-generated method stub
-    return null;
+    Date endDate = getEndOfMonth(amortizationStartDate);
+    
+    Amortization result = OBProvider.getInstance().get(Amortization.class);
+    result.setOrganization(organization);
+    result.setStartingDate(amortizationStartDate);
+    result.setEndingDate(endDate);
+    result.setAccountingDate(endDate);
+    
+    result.setName(amortizationStartDate.toString());
+    
+    return result;
+  }
+
+  private Date getEndOfMonth(Date date) {
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTime(date);
+    calendar.add(Calendar.MONTH, 1);
+    calendar.set(Calendar.DAY_OF_MONTH, 1);
+    calendar.add(Calendar.DAY_OF_MONTH, -1);
+    return calendar.getTime();
   }
 
   private Map<Date, Amortization> getUnprocessedAmortizationHeader(Organization organization) {
@@ -197,7 +250,6 @@ public class AssetAmortizationProcess extends DalBaseProcess {
 
   private Result isAssetEligibleForDepreciation(Asset asset) {
 
-    Long uselifeyear = asset.getUsableLifeYears() != null ? asset.getUsableLifeYears() : 0;
     Long uselifemonth = asset.getUsableLifeMonths() != null ? asset.getUsableLifeMonths() : 0;
 
     DepreciationPeriod period = DepreciationPeriod.valueOf(asset.getAmortize());
@@ -207,13 +259,13 @@ public class AssetAmortizationProcess extends DalBaseProcess {
         .and(asset.getCurrency() != null, "currency date is empty.")
         .and(asset.isDepreciate(), "asset is not deprecited, no amortization created")
         .and(ifDepreciationTypeIsDoubleDecliningThenValidate(asset),
-            "deprecication type is double declininig, but annual depreication percentage is empty/zero/negative.")
+            "deprecication type is double declininig, but annual depreication percentage is NOT a positive number or use life month is not match with given annual depreciation percentage.")
         .and(ifCalculcationTypeIsPercentageThenValidate(asset),
-            "calculation type is percentage, but annual depreication percentage is empty/zero/negative.")
-        .andNot(period == DepreciationPeriod.MO && uselifemonth <= 0,
-            "amortize (depreciation period) monthly, but use life month is empty/zero/negative.")
-        .andNot(period == DepreciationPeriod.YE && uselifeyear <= 0,
-            "amortize (depreciation period) yearly, but use life year is empty/zero/negative.")
+            "calculation type is percentage, but annual depreication percentage is NOT a positive number.")
+        .and(period == DepreciationPeriod.MO,
+            "depreciation period is not monthly. now only monthly depreciation are supported.")
+        .and(uselifemonth > 0,
+            "use life month must be positive integer number.")
         .evaluate();
   }
 
@@ -231,17 +283,63 @@ public class AssetAmortizationProcess extends DalBaseProcess {
   }
 
   private boolean ifDepreciationTypeIsDoubleDecliningThenValidate(Asset asset) {
+    if (!isAssetDepreciatedUsingDoubleDeclining(asset))
+      return false;
+    
+    if (!isAssetHasValidUseLifeYear(asset)){
+      return false;
+    }
+    
+    return true;
+  }
 
+  private boolean isAssetHasValidUseLifeYear(Asset asset) {
+    
+    if (!isAssetHasValidAnnualDepreciationPercentage(asset))
+      return false;
+    
+    Long assetTimeSpanInMonth = asset.getUsableLifeMonths();    
+    if (assetTimeSpanInMonth==null)
+      return false;
+    
+    int annualdepreciationpercentage = asset.getAnnualDepreciation().intValue();
+    
+    if (annualdepreciationpercentage==50 && assetTimeSpanInMonth!=48)
+      return false;
+    if (annualdepreciationpercentage==25 && assetTimeSpanInMonth!=96)
+      return false;
+    if (annualdepreciationpercentage==20 && assetTimeSpanInMonth!=120)
+      return false;
+    
+    return true;
+  }
+
+  private boolean isAssetHasValidAnnualDepreciationPercentage(Asset asset) {
+    if (asset.getAnnualDepreciation()==null)
+      return false;
+    
+    int annualdepreciationpercentage = asset.getAnnualDepreciation().intValue();
+    if (annualdepreciationpercentage<0)
+      return false;
+    
+    switch (annualdepreciationpercentage) {
+      case 50:
+      case 25:
+      case 20:
+        return true;
+
+      default:
+        return false;
+    }
+  }
+
+  private boolean isAssetDepreciatedUsingDoubleDeclining(Asset asset) {
     DepreciationMethod depreciationmethod = DepreciationMethod.valueOf(asset.getDepreciationType());
-
-    if (depreciationmethod != DepreciationMethod.CAM_DOUBLEDECLINING) {
+    if (depreciationmethod == DepreciationMethod.CAM_DOUBLEDECLINING) {
       return true;
     }
-
-    BigDecimal annualdepreciationpercentage = asset.getAnnualDepreciation();
-
-    return ObjectUtils.compare(annualdepreciationpercentage, BigDecimal.ZERO) >= 0;
-
+    
+    return false;
   }
 
   private void removePendingAmortizationLine(Asset asset) {
